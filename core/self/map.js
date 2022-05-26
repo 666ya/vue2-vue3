19196a47e4dd315f56657ca4712a54664de1b293
焦文儒 <jiaowr@louddt.com>
2022-05-26 17:27:38 +0800

    fix: vue3源码applyOptions example

--- core/self/map.js
+++ core/self/map.js
@@ -1,14 +1,40 @@
-var camelizeRE = /-(\w)/g;
-var fun = function (str) {
-    return str.replace(camelizeRE, function (_, c) { return (c ? c.toUpperCase() : ''); });
-};
+// var camelizeRE = /-(\w)/g;
+// var fun = function (str) {
+//     return str.replace(camelizeRE, function (_, c) { return (c ? c.toUpperCase() : ''); });
+// };
 
 
-const hyphenateRE = /\B([A-Z])/g
+// const hyphenateRE = /\B([A-Z])/g
 
-const hyphenate = (str) => str.replace(hyphenateRE, "-$1").toLowerCase()
+// const hyphenate = (str) => str.replace(hyphenateRE, "-$1").toLowerCase()
 
 
-console.log(hyphenate('ABcDc'))
-console.log(hyphenate('true'))
-console.log(hyphenate('测试'))
+// console.log(hyphenate('ABcDc'))
+// console.log(hyphenate('true'))
+// console.log(hyphenate('测试'))
+
+
+
+var obj = {
+    bar: 'bar'
+}
+
+const proxy = new Proxy(obj, {
+    get(target, key, reciver) { 
+        console.log('get propxy')
+        return target[key]
+    },
+    defineProperty(target, key, descriptor) { 
+        return Reflect.defineProperty(target, key, descriptor)
+    }
+})
+
+Object.defineProperty(proxy, 'foo', {
+    enumerable: true,
+    configurable: true,
+    get() { 
+        return obj.bar
+    }
+})
+
+// console.log(proxy.bar)
