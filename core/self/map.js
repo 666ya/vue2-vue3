73b72736222d8d58beff69df362ed4d5a969ded4
焦文儒 <jiaowr@louddt.com>
2022-07-20 16:53:41 +0800

    fix: 修改

--- core/self/map.js
+++ core/self/map.js
@@ -16,25 +16,48 @@
 
 
 var obj = {
-    bar: 'bar'
+    book: {
+        name: '名字',
+        year: '2017',
+
+    }
 }
 
 const proxy = new Proxy(obj, {
-    get(target, key, reciver) { 
+    get(target, key, reciver) {
         console.log('get propxy')
+        console.log(key)
         return target[key]
     },
-    defineProperty(target, key, descriptor) { 
-        return Reflect.defineProperty(target, key, descriptor)
-    }
+    // defineProperty(target, key, descriptor) {
+    //     return Reflect.defineProperty(target, key, descriptor)
+    // }
 })
+console.log(proxy.book.name)
 
-Object.defineProperty(proxy, 'foo', {
-    enumerable: true,
-    configurable: true,
-    get() { 
-        return obj.bar
-    }
-})
+
+// Object.defineProperty(proxy, 'foo', {
+//     enumerable: true,
+//     configurable: true,
+//     get() {
+//         return obj.bar
+//     }
+// })
 
 // console.log(proxy.bar)
+
+
+// class ComputedRefImpl { 
+//     _value = '2'
+//     _dirty = true
+//     get value() { 
+//         this._dirty = false
+//         return this._value
+//     }
+//     set value(newValue) { 
+//         this._value = newValue
+//     }
+// }
+// const cref = new ComputedRefImpl()
+
+// console.log(cref)
