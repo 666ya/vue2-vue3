518b90ca7532191dca24394c7129d0e1ba105ee0
焦文儒 <jiaowr@louddt.com>
2022-10-25 17:22:14 +0800

    fix: renderer完善

--- core/reactivity/renderer.js
+++ core/reactivity/renderer.js
@@ -1,59 +1,120 @@
-function createRenderer(options) {
-    const {
-        createElement,
-        setElementText,
-        insert
-    } = options
+  function shouldSetAsProps(el, key, value) {
+      if (key === 'form' && el.tagName === 'INPUT') return false
+      return key in el
+  }
 
-    function mountElement(vnode, container) {
-        const el = createElement(vnode.type)
-        if (typeof vnode.children === 'string') {
-            // el.textContent = vnode.children
-            setElementText(el, vnode.children)
-        } else if (Array.isArray(vnode.children)) {
-            vnode.children.forEach(child => {
-                path(null, child, el)
-            })
-        }
-        if (vnode.props) {
-            for (const key in vnode.props) {
-                if (key in el) {
-                    const type = typeof el[key]
-                    const value = vnode.props[key]
-                    if (type === 'boolean' && value === '') {
-                        el[key] = true
-                    } else {
-                        el[key] = value
-                    }
-                } else {
-                    el.setAttribute(key, vnode.props[key])
-                }
+  function createRenderer(options) {
+      const {
+          createElement,
+          setElementText,
+          patchProps,
+          insert
+      } = options
 
-            }
-        }
-        insert(el, container)
-        // container.appendChild(el)
-    }
+      function patchChildren(n1, n2, container) {
+          if (typeof n2.children === 'string') {
+              if (Array.isArray(n1.children)) {
+                  n1.children.forEach(c => umount(c))
+              }
+              setElementText(container, n2.children)
+          } else if (Array.isArray(n2.children)) {
+              if (Array.isArray(n1.children)) {
+                  // diff算法
+              } else {
+                  setElementText(container, '')
+                  n2.children.forEach(c => patchChildren(null, c, container))
+              }
+          } else {
+              if (Array.isArray(n1.children)) {
+                  n1.children.forEach(c => umount(c))
+              } else if (typeof n1.children === 'string') {
+                  setElementText(container, '')
+              }
+          }
+      }
 
-    function path(n1, n2, container) {
-        if (!n1) {
-            mountElement(n2, container)
-        } else {
-            // 打补丁
-        }
-    }
+      function mountElement(vnode, container) {
+          const el = vnode.el = createElement(vnode.type)
+          if (typeof vnode.children === 'string') {
+              // el.textContent = vnode.children
+              setElementText(el, vnode.children)
+          } else if (Array.isArray(vnode.children)) {
+              vnode.children.forEach(child => {
+                  path(null, child, el)
+              })
+          }
+          if (vnode.props) {
+              for (const key in vnode.props) {
+                  patchProps(el, key, null, vnode.props[key])
+              }
+          }
+          insert(el, container)
+          // container.appendChild(el)
+      }
 
-    function render(vnode, container) {
-        if (vnode) {
-            path(container._vnode, vnode, container)
-        } else {
-            if (container._vnode) {
-                container.innerHTML = ''
-            }
-        }
-        container._vnode = vnode
-    }
-    return {
-        render
-    }
-}
+      function patchElement(n1, n2) {
+          const el = n2.el = n1.el
+          const oldProps = n1.props
+          const newProps = n2.props
+          // 更新props
+          for (const key in newProps) {
+              if (newProps[key] !== oldProps[key]) {
+                  patchProps(el, key, oldProps[key], newProps[key])
+              }
+          }
+          for (const key in oldProps) {
+              if (!(key in newProps)) {
+                  patchProps(el, key, oldProps[key], null)
+              }
+          }
+          patchChildren(n1, n2, el)
+      }
+
+      function path(n1, n2, container) {
+          if (n1 && n1.type !== n2.type) {
+              umount(n1)
+              n1 = null
+          }
+          const {
+              type
+          } = n2
+          if (typeof type === 'string') {
+              if (!n1) {
+                  mountElement(n2, container)
+              } else {
+                  patchElement(n1, n2)
+              }
+          } else if (type === Text) {
+              // 文本节点
+          } else if (typeof type === 'object') {
+              // 组件
+          } else if (type === 'xxxx') {
+              // 其他类型node
+          }
+      }
+
+      function umount(vnode) {
+          const parent = vnode.el.parentNode
+          if (parent) {
+              parent.removeChild(vnode.el)
+          }
+      }
+
+      function render(vnode, container) {
+          if (vnode) {
+              path(container._vnode, vnode, container)
+          } else {
+              if (container._vnode) {
+                  umount(container._vnode)
+                  // const el = container._vnode.el
+                  // const parent = el.parentNode
+                  // if (parent) parent.removeChild(el)
+                  // container.innerHTML = ''
+              }
+          }
+          container._vnode = vnode
+      }
+      return {
+          render
+      }
+  }
--- core/src/renderer.js
+++ core/src/renderer.js
@@ -1,5 +1,7 @@
 const count = ref(1)
-var options = {
+const bol = ref(true)
+
+const renderer = createRenderer({
     createElement(tag) {
         return document.createElement(tag)
     },
@@ -8,24 +10,83 @@ var options = {
     },
     setElementText(el, text) {
         el.textContent = text
-    }
-}
-const renderer = createRenderer(options)
-const vnode = {
-    type: 'p',
-    props: {
-        id: 'foo',
     },
-    children: [{
+    patchProps(el, key, preveValue, nextValue) {
+        if (/^on/.test(key)) {
+            const invokers = el._vei || (el._vei = {})
+            let invoker = invokers[key]
+            const name = key.slice(2).toLowerCase()
+            if (nextValue) {
+                if (!invoker) {
+                    invoker = el._vei[key] = (e) => {
+                        console.log(e.timeStamp)
+                        console.log(invoker.attached)
+                        if (e.timeStamp < invoker.attached) return
+                        if (Array.isArray(invoker.value)) {
+                            invoker.value.forEach(fn => fn(e))
+                        } else {
+                            invoker.value(e)
+                        }
+                    }
+                    invoker.value = nextValue
+                    invoker.attached = performance.now()
+                    el.addEventListener(name, invoker)
+                } else {
+                    invoker.value = nextValue
+                }
+
+            } else if (invoker) {
+                el.removeEventListener(name, invoker)
+            }
+        } else if (key === 'class') {
+            el.classNamee = nextValue || ''
+        } else if (shouldSetAsProps(el, key, nextValue)) {
+            console.log(el)
+            const type = typeof el[key]
+            if (type === 'boolean' && nextValue === '') {
+                el[key] = true
+            } else {
+                el[key] = nextValue
+            }
+        } else {
+            el.setAttribute(key, nextValue)
+        }
+    },
+})
+
+const Text = Symbol()
+const newVnode = {
+    type: Text,
+    children: '我是文本内容'
+}
+const Comment = Symbol()
+effect(() => {
+    const vnode = {
         type: 'input',
         props: {
-            disabled: ''
+            disabled: false
         }
-    }]
-}
-
-
-effect(() => {
+    }
+    // const vnode = {
+    //     type: 'div',
+    //     props: bol.value ? {
+    //         id: 'foo',
+    //         onClick: [
+    //             () => {
+    //                 alert('父元素click')
+    //             }
+    //         ],
+    //     } : {},
+    //     children: bol.value ? [{
+    //         type: 'p',
+    //         props: {
+    //             onClick: () => {
+    //                 bol.value = false
+    //             }
+    //         },
+    //         children: 'text'
+    //     }] : 'hello world'
+    // }
     renderer.render(vnode, document.getElementById('app'))
 })
 
