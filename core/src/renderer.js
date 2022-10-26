7e64c12c4689108970486c980bd85343ac786331
jiaowenru <j_wrsummer@163.com>
2022-10-26 22:01:54 +0800

    feat: vue.js设计与实现第7章代码

--- core/src/renderer.js
+++ core/src/renderer.js
@@ -1,95 +1,24 @@
-const count = ref(1)
-const bol = ref(true)
-
-const renderer = createRenderer({
+const options = {
     createElement(tag) {
         return document.createElement(tag)
-    },
-    insert(el, parent, anchor = null) {
-        return parent.insertBefore(el, anchor)
     },
     setElementText(el, text) {
         el.textContent = text
     },
-    patchProps(el, key, preveValue, nextValue) {
-        if (/^on/.test(key)) {
-            const invokers = el._vei || (el._vei = {})
-            let invoker = invokers[key]
-            const name = key.slice(2).toLowerCase()
-            if (nextValue) {
-                if (!invoker) {
-                    invoker = el._vei[key] = (e) => {
-                        console.log(e.timeStamp)
-                        console.log(invoker.attached)
-                        if (e.timeStamp < invoker.attached) return
-                        if (Array.isArray(invoker.value)) {
-                            invoker.value.forEach(fn => fn(e))
-                        } else {
-                            invoker.value(e)
-                        }
-                    }
-                    invoker.value = nextValue
-                    invoker.attached = performance.now()
-                    el.addEventListener(name, invoker)
-                } else {
-                    invoker.value = nextValue
-                }
-
-            } else if (invoker) {
-                el.removeEventListener(name, invoker)
-            }
-        } else if (key === 'class') {
-            el.classNamee = nextValue || ''
-        } else if (shouldSetAsProps(el, key, nextValue)) {
-            console.log(el)
-            const type = typeof el[key]
-            if (type === 'boolean' && nextValue === '') {
-                el[key] = true
-            } else {
-                el[key] = nextValue
-            }
-        } else {
-            el.setAttribute(key, nextValue)
-        }
-    },
-})
-
-const Text = Symbol()
-const newVnode = {
-    type: Text,
-    children: '我是文本内容'
-}
-const Comment = Symbol()
-effect(() => {
-    const vnode = {
-        type: 'input',
-        props: {
-            disabled: false
-        }
+    insert(el, parent, anchor) {
+        parent.insertBefore(el, anchor)
     }
-    // const vnode = {
-    //     type: 'div',
-    //     props: bol.value ? {
-    //         id: 'foo',
-    //         onClick: [
-    //             () => {
-    //                 alert('父元素click')
-    //             }
-    //         ],
-    //     } : {},
-    //     children: bol.value ? [{
-    //         type: 'p',
-    //         props: {
-    //             onClick: () => {
-    //                 bol.value = false
-    //             }
-    //         },
-    //         children: 'text'
-    //     }] : 'hello world'
-    // }
-    renderer.render(vnode, document.getElementById('app'))
-})
 
-// setTimeout(() => {
-//     count.value++
-// }, 2000)
+}
+const render = createRenderer(options).render
+const vnode = {
+    type: 'p',
+    children: '渲染器'
+}
+// effect(() => {
+//     render(vnode, document.getElementById('app'))
+// })
+render(vnode, document.getElementById('app'))
+setTimeout(() => {
+    render(null, document.getElementById('app'))
+}, 2000)
